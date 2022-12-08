cec3fbb3525df19227e0b5aaef61cb4486ccc335
al-mahfuz <almahfuzhossen@gmail.com>
2022-12-08 16:29:43 +0600

    functional bank

--- js/functional.js
+++ js/functional.js
@@ -5,3 +5,29 @@ function getInputValue(fieldId) {
     inputField.value = '';
     return value;
 }
+
+function getInnerTextValue(fieldId) {
+    const fieldTag = document.getElementById(fieldId);
+    const fieldValueInText = fieldTag.innerText;
+    const value = parseFloat(fieldValueInText);
+    return value;
+}
+
+function updateTotal(fieldId, amount) {
+    const previousTotal = getInnerTextValue(fieldId);
+    const newTotal = previousTotal + amount;
+    document.getElementById(fieldId).innerText = newTotal;
+    return newTotal;
+}
+
+function updateBalance(amount, isAdding) {
+    const previousBalance = getInnerTextValue('balance-total');
+    let newBalance;
+    if (isAdding == true) {
+        newBalance = previousBalance + amount;
+    }
+    else {
+        newBalance = previousBalance - amount;
+    }
+    document.getElementById('balance-total').innerText = newBalance;
+}
