47d85be8c5eda32b17c6142d483faaeba58c5fb7
al-mahfuz <almahfuzhossen@gmail.com>
2022-12-10 11:58:54 +0600

    players

---
+++ js/players.js
@@ -0,0 +1,8 @@
+const players = document.getElementsByClassName('player');
+for (const player of players) {
+    console.log(player);
+    player.style.border = '1px solid red';
+    player.style.borderRadius = '15px'
+    player.style.margin = '10px';
+    player.style.padding = '10px';
+}
